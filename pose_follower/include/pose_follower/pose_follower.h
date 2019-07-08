56d7346db8f490f44de60ae7753a786a3c43aa5e
PaperFanz <petefan.7@gmail.com>
2019-07-08 13:06:48 -0500

    update to melodic

--- pose_follower/include/pose_follower/pose_follower.h
+++ pose_follower/include/pose_follower/pose_follower.h
@@ -37,51 +37,24 @@
 #ifndef POSE_FOLLOWER_POSE_FOLLOWER_H_
 #define POSE_FOLLOWER_POSE_FOLLOWER_H_
 #include <ros/ros.h>
-#include <tf/tf.h>
-#include <tf/transform_datatypes.h>
-#include <tf/transform_listener.h>
+#include <tf2/convert.h>
+#include <tf2/transform_datatypes.h>
+#include <tf2/utils.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/buffer.h>
 #include <nav_core/base_local_planner.h>
 #include <costmap_2d/costmap_2d_ros.h>
+#include <geometry_msgs/Pose.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Twist.h>
 #include <nav_msgs/Odometry.h>
 #include <base_local_planner/trajectory_planner_ros.h>
 
-class lpf
-{
-  public:
-    float filter(const float& new_msrmt);
-    float c_ = 1.0; // Related to the cutoff frequency of the filter.
-      // c=1 results in a cutoff at 1/4 of the sampling rate.
-      // See bitbucket.org/AndyZe/pid if you want to get more sophisticated.
-      // Larger c --> trust the filtered data more, trust the measurements less --> lower cutoff frequency
-
-  private:
-    float prev_msrmts_ [3] = {0., 0., 0.};
-    float prev_filtered_msrmts_ [2] = {0., 0.};
-};
-
-float lpf::filter(const float& new_msrmt)
-{
-  // Push in the new measurement
-  prev_msrmts_[2] = prev_msrmts_[1];
-  prev_msrmts_[1] = prev_msrmts_[0];
-  prev_msrmts_[0] = new_msrmt;
-
-  float new_filtered_msrmt = (1/(1+c_*c_+1.414*c_))*(prev_msrmts_[2]+2*prev_msrmts_[1]+prev_msrmts_[0]-(c_*c_-1.414*c_)*prev_filtered_msrmts_[1]-(-2*c_*c_+2)*prev_filtered_msrmts_[0]);;
-
-  // Store the new filtered measurement
-  prev_filtered_msrmts_[1] = prev_filtered_msrmts_[0];
-  prev_filtered_msrmts_[0] = new_filtered_msrmt;
-
-  return new_filtered_msrmt;
-}
-
 namespace pose_follower {
   class PoseFollower : public nav_core::BaseLocalPlanner {
     public:
       PoseFollower();
-      void initialize(std::string name, tf::TransformListener* tf, costmap_2d::Costmap2DROS* costmap_ros);
+      void initialize(std::string name, tf2_ros::Buffer* tf, costmap_2d::Costmap2DROS* costmap_ros);
       bool isGoalReached();
       bool setPlan(const std::vector<geometry_msgs::PoseStamped>& global_plan);
       bool computeVelocityCommands(geometry_msgs::Twist& cmd_vel);
@@ -91,20 +64,22 @@ namespace pose_follower {
         return n < 0.0 ? -1.0 : 1.0;
       }
 
-      geometry_msgs::Twist diff2D(const tf::Pose& pose1, const tf::Pose&  pose2);
+      geometry_msgs::Twist diff2D(const geometry_msgs::Pose& pose1, const geometry_msgs::Pose&  pose2);
       geometry_msgs::Twist limitTwist(const geometry_msgs::Twist& twist);
       double headingDiff(double pt_x, double pt_y, double x, double y, double heading);
 
-      bool transformGlobalPlan(const tf::TransformListener& tf, const std::vector<geometry_msgs::PoseStamped>& global_plan, 
+      bool transformGlobalPlan(const tf2_ros::Buffer& tf, const std::vector<geometry_msgs::PoseStamped>& global_plan,
           const costmap_2d::Costmap2DROS& costmap, const std::string& global_frame,
           std::vector<geometry_msgs::PoseStamped>& transformed_plan);
 
       void odomCallback(const nav_msgs::Odometry::ConstPtr& msg);
       bool stopped();
+      void publishPlan(const std::vector<geometry_msgs::PoseStamped> &path, const ros::Publisher &pub);
 
-      tf::TransformListener* tf_;
+      tf2_ros::Buffer* tf_;
       costmap_2d::Costmap2DROS* costmap_ros_;
       ros::Publisher vel_pub_;
+      ros::Publisher global_plan_pub_;
       double K_trans_, K_rot_, tolerance_trans_, tolerance_rot_;
       double tolerance_timeout_;
       double max_vel_lin_, max_vel_th_;
@@ -123,13 +98,6 @@ namespace pose_follower {
       std::vector<geometry_msgs::PoseStamped> global_plan_;
       base_local_planner::TrajectoryPlannerROS collision_planner_;
       int samples_;
-      // Low pass filter
-      lpf linear_x_filter_;
-      lpf linear_y_filter_;
-      lpf linear_z_filter_;
-      lpf rot_x_filter_;
-      lpf rot_y_filter_;
-      lpf rot_z_filter_;
   };
 };
 #endif
--- pose_follower/src/pose_follower.cpp
+++ pose_follower/src/pose_follower.cpp
@@ -1,3 +1,4 @@
+
 /*********************************************************************
 *
 * Software License Agreement (BSD License)
@@ -34,15 +35,16 @@
 *
 * Author: Eitan Marder-Eppstein
 *********************************************************************/
+#include <nav_msgs/Path.h>
 #include <pose_follower/pose_follower.h>
-#include <pluginlib/class_list_macros.h>
+#include <pluginlib/class_list_macros.hpp>
 
-PLUGINLIB_DECLARE_CLASS(pose_follower, PoseFollower, pose_follower::PoseFollower, nav_core::BaseLocalPlanner)
+PLUGINLIB_EXPORT_CLASS(pose_follower::PoseFollower, nav_core::BaseLocalPlanner)
 
 namespace pose_follower {
   PoseFollower::PoseFollower(): tf_(NULL), costmap_ros_(NULL) {}
 
-  void PoseFollower::initialize(std::string name, tf::TransformListener* tf, costmap_2d::Costmap2DROS* costmap_ros){
+  void PoseFollower::initialize(std::string name, tf2_ros::Buffer* tf, costmap_2d::Costmap2DROS* costmap_ros){
     tf_ = tf;
     costmap_ros_ = costmap_ros;
     current_waypoint_ = 0;
@@ -96,6 +98,8 @@ namespace pose_follower {
     //if turn_in_place_first is true, turn in place if our heading is more than this far from facing the goal location
     node_private.param("max_heading_diff_before_moving", max_heading_diff_before_moving_, 0.17);
 
+    global_plan_pub_ = node_private.advertise<nav_msgs::Path>("global_plan", 1);
+
     ros::NodeHandle node;
     odom_sub_ = node.subscribe<nav_msgs::Odometry>("odom", 1, boost::bind(&PoseFollower::odomCallback, this, _1));
     vel_pub_ = node.advertise<geometry_msgs::Twist>("cmd_vel", 10);
@@ -142,9 +146,28 @@ namespace pose_follower {
       && fabs(base_odom.twist.twist.linear.y) <= trans_stopped_velocity_;
   }
 
+  void PoseFollower::publishPlan(const std::vector<geometry_msgs::PoseStamped> &path,
+                               const ros::Publisher &pub) {
+    // given an empty path we won't do anything
+    if (path.empty())
+      return;
+
+    // create a path message
+    nav_msgs::Path gui_path;
+    gui_path.poses.resize(path.size());
+    gui_path.header.frame_id = path[0].header.frame_id;
+    gui_path.header.stamp = path[0].header.stamp;
+
+    // Extract the plan in world co-ordinates, we assume the path is all in the same frame
+    for (unsigned int i = 0; i < path.size(); i++) {
+      gui_path.poses[i] = path[i];
+    }
+    pub.publish(gui_path);
+  }
+
   bool PoseFollower::computeVelocityCommands(geometry_msgs::Twist& cmd_vel){
     //get the current pose of the robot in the fixed frame
-    tf::Stamped<tf::Pose> robot_pose;
+    geometry_msgs::PoseStamped robot_pose;
     if(!costmap_ros_->getRobotPose(robot_pose)){
       ROS_ERROR("Can't get robot pose");
       geometry_msgs::Twist empty_twist;
@@ -153,14 +176,12 @@ namespace pose_follower {
     }
 
     //we want to compute a velocity command based on our current waypoint
-    tf::Stamped<tf::Pose> target_pose;
-    tf::poseStampedMsgToTF(global_plan_[current_waypoint_], target_pose);
-
-    ROS_DEBUG("PoseFollower: current robot pose %f %f ==> %f", robot_pose.getOrigin().x(), robot_pose.getOrigin().y(), tf::getYaw(robot_pose.getRotation()));
-    ROS_DEBUG("PoseFollower: target robot pose %f %f ==> %f", target_pose.getOrigin().x(), target_pose.getOrigin().y(), tf::getYaw(target_pose.getRotation()));
+    geometry_msgs::PoseStamped target_pose = global_plan_[current_waypoint_];
+    ROS_DEBUG("PoseFollower: current robot pose %f %f ==> %f", robot_pose.pose.position.x, robot_pose.pose.position.y, tf2::getYaw(robot_pose.pose.orientation));
+    ROS_DEBUG("PoseFollower: target robot pose %f %f ==> %f", target_pose.pose.position.x, target_pose.pose.position.y, tf2::getYaw(target_pose.pose.orientation));
 
     //get the difference between the two poses
-    geometry_msgs::Twist diff = diff2D(target_pose, robot_pose);
+    geometry_msgs::Twist diff = diff2D(target_pose.pose, robot_pose.pose);
     ROS_DEBUG("PoseFollower: diff %f %f ==> %f", diff.linear.x, diff.linear.y, diff.angular.z);
 
     geometry_msgs::Twist limit_vel = limitTwist(diff);
@@ -201,14 +222,11 @@ namespace pose_follower {
           fabs(diff.linear.y) <= tolerance_trans_ &&
 	  fabs(diff.angular.z) <= tolerance_rot_)
     {
-      // We're close enough, despite not being at the final waypoint. Stop here.
       if(current_waypoint_ < global_plan_.size() - 1)
       {
         current_waypoint_++;
-        tf::poseStampedMsgToTF(global_plan_[current_waypoint_], target_pose);
-        diff = diff2D(target_pose, robot_pose);
-        geometry_msgs::Twist empty_twist;
-        cmd_vel = empty_twist; // Stop
+        target_pose = global_plan_[current_waypoint_];
+        diff = diff2D(target_pose.pose, robot_pose.pose);
       }
       else
       {
@@ -228,15 +246,6 @@ namespace pose_follower {
       cmd_vel = empty_twist;
     }
 
-    // Filter the command
-    cmd_vel.linear.x = linear_x_filter_.filter(cmd_vel.linear.x);
-    cmd_vel.linear.y = linear_y_filter_.filter(cmd_vel.linear.y);
-    cmd_vel.linear.z = linear_z_filter_.filter(cmd_vel.linear.z);
-
-    cmd_vel.angular.x = rot_x_filter_.filter(cmd_vel.angular.x);
-    cmd_vel.angular.y = rot_x_filter_.filter(cmd_vel.angular.y);
-    cmd_vel.angular.z = rot_x_filter_.filter(cmd_vel.angular.z);
-
     return true;
   }
 
@@ -247,23 +256,27 @@ namespace pose_follower {
       ROS_ERROR("Could not transform the global plan to the frame of the controller");
       return false;
     }
+
+    ROS_DEBUG("global plan size: %lu", global_plan_.size());
+    publishPlan(global_plan_, global_plan_pub_);
     return true;
   }
 
   bool PoseFollower::isGoalReached(){
-    if(goal_reached_time_ + ros::Duration(tolerance_timeout_) < ros::Time::now() && stopped()){
-      return true;
-    }
-    return false;
+    return goal_reached_time_ + ros::Duration(tolerance_timeout_) < ros::Time::now() && stopped();
   }
 
-  geometry_msgs::Twist PoseFollower::diff2D(const tf::Pose& pose1, const tf::Pose& pose2)
+  geometry_msgs::Twist PoseFollower::diff2D(const geometry_msgs::Pose& pose1_msg,
+                                            const geometry_msgs::Pose& pose2_msg)
   {
+    tf2::Transform pose1, pose2;
+    tf2::convert(pose1_msg, pose1);
+    tf2::convert(pose2_msg, pose2);
     geometry_msgs::Twist res;
-    tf::Pose diff = pose2.inverse() * pose1;
+    tf2::Transform diff = pose2.inverse() * pose1;
     res.linear.x = diff.getOrigin().x();
     res.linear.y = diff.getOrigin().y();
-    res.angular.z = tf::getYaw(diff.getRotation());
+    res.angular.z = tf2::getYaw(diff.getRotation());
 
     if(holonomic_ || (fabs(res.linear.x) <= tolerance_trans_ && fabs(res.linear.y) <= tolerance_trans_))
       return res;
@@ -273,13 +286,13 @@ namespace pose_follower {
     
     //we want to compute a goal based on the heading difference between our pose and the target
     double yaw_diff = headingDiff(pose1.getOrigin().x(), pose1.getOrigin().y(), 
-        pose2.getOrigin().x(), pose2.getOrigin().y(), tf::getYaw(pose2.getRotation()));
+        pose2.getOrigin().x(), pose2.getOrigin().y(), tf2::getYaw(pose2.getRotation()));
 
     //we'll also check if we can move more effectively backwards
     if (allow_backwards_) 
     {
       double neg_yaw_diff = headingDiff(pose1.getOrigin().x(), pose1.getOrigin().y(), 
-					pose2.getOrigin().x(), pose2.getOrigin().y(), M_PI + tf::getYaw(pose2.getRotation()));
+					pose2.getOrigin().x(), pose2.getOrigin().y(), M_PI + tf2::getYaw(pose2.getRotation()));
 
       //check if its faster to just back up
       if(fabs(neg_yaw_diff) < fabs(yaw_diff)){
@@ -289,15 +302,16 @@ namespace pose_follower {
     }
 
     //compute the desired quaterion
-    tf::Quaternion rot_diff = tf::createQuaternionFromYaw(yaw_diff);
-    tf::Quaternion rot = pose2.getRotation() * rot_diff;
-    tf::Pose new_pose = pose1;
+    tf2::Quaternion rot_diff;
+    rot_diff.setRPY(0.0, 0.0, yaw_diff);
+    tf2::Quaternion rot = pose2.getRotation() * rot_diff;
+    tf2::Transform new_pose = pose1;
     new_pose.setRotation(rot);
 
     diff = pose2.inverse() * new_pose;
     res.linear.x = diff.getOrigin().x();
     res.linear.y = diff.getOrigin().y();
-    res.angular.z = tf::getYaw(diff.getRotation());
+    res.angular.z = tf2::getYaw(diff.getRotation());
     return res;
   }
 
@@ -317,6 +331,7 @@ namespace pose_follower {
     {
       res.linear.x = 0;
       res.linear.y = 0;
+      if (fabs(res.angular.z) > max_vel_th_) res.angular.z = max_vel_th_ * sign(res.angular.z);
       if (fabs(res.angular.z) < min_in_place_vel_th_) res.angular.z = min_in_place_vel_th_ * sign(res.angular.z);
       return res;
     }
@@ -339,6 +354,10 @@ namespace pose_follower {
 
     if (fabs(res.angular.z) > max_vel_th_) res.angular.z = max_vel_th_ * sign(res.angular.z);
     if (fabs(res.angular.z) < min_vel_th_) res.angular.z = min_vel_th_ * sign(res.angular.z);
+    if (std::isnan(res.linear.x))
+        res.linear.x = 0.0;
+    if (std::isnan(res.linear.y))
+        res.linear.y = 0.0;
 
     //we want to check for whether or not we're desired to rotate in place
     if(sqrt(twist.linear.x * twist.linear.x + twist.linear.y * twist.linear.y) < in_place_trans_vel_){
@@ -351,7 +370,7 @@ namespace pose_follower {
     return res;
   }
 
-  bool PoseFollower::transformGlobalPlan(const tf::TransformListener& tf, const std::vector<geometry_msgs::PoseStamped>& global_plan, 
+  bool PoseFollower::transformGlobalPlan(const tf2_ros::Buffer& tf, const std::vector<geometry_msgs::PoseStamped>& global_plan,
       const costmap_2d::Costmap2DROS& costmap, const std::string& global_frame,
       std::vector<geometry_msgs::PoseStamped>& transformed_plan){
     const geometry_msgs::PoseStamped& plan_pose = global_plan[0];
@@ -359,42 +378,44 @@ namespace pose_follower {
     transformed_plan.clear();
 
     try{
-      if (!global_plan.size() > 0)
+      if (global_plan.empty())
       {
         ROS_ERROR("Recieved plan with zero length");
         return false;
       }
 
-      tf::StampedTransform transform;
-      tf.lookupTransform(global_frame, ros::Time(), 
-          plan_pose.header.frame_id, plan_pose.header.stamp, 
-          plan_pose.header.frame_id, transform);
+      geometry_msgs::TransformStamped transform;
+      transform = tf.lookupTransform(global_frame, ros::Time(),
+                                     plan_pose.header.frame_id, plan_pose.header.stamp,
+                                     plan_pose.header.frame_id);
+      tf2::Stamped<tf2::Transform> tf_transform;
+      tf2::convert(transform, tf_transform);
 
-      tf::Stamped<tf::Pose> tf_pose;
+      tf2::Stamped<tf2::Transform> tf_pose;
       geometry_msgs::PoseStamped newer_pose;
       //now we'll transform until points are outside of our distance threshold
       for(unsigned int i = 0; i < global_plan.size(); ++i){
         const geometry_msgs::PoseStamped& pose = global_plan[i];
-        poseStampedMsgToTF(pose, tf_pose);
-        tf_pose.setData(transform * tf_pose);
-        tf_pose.stamp_ = transform.stamp_;
+        tf2::convert(pose, tf_pose);
+        tf_pose.setData(tf_transform * tf_pose);
+        tf_pose.stamp_ = tf_transform.stamp_;
         tf_pose.frame_id_ = global_frame;
-        poseStampedTFToMsg(tf_pose, newer_pose);
+        tf2::toMsg(tf_pose, newer_pose);
 
         transformed_plan.push_back(newer_pose);
       }
     }
-    catch(tf::LookupException& ex) {
+    catch(tf2::LookupException& ex) {
       ROS_ERROR("No Transform available Error: %s\n", ex.what());
       return false;
     }
-    catch(tf::ConnectivityException& ex) {
+    catch(tf2::ConnectivityException& ex) {
       ROS_ERROR("Connectivity Error: %s\n", ex.what());
       return false;
     }
-    catch(tf::ExtrapolationException& ex) {
+    catch(tf2::ExtrapolationException& ex) {
       ROS_ERROR("Extrapolation Error: %s\n", ex.what());
-      if (global_plan.size() > 0)
+      if (!global_plan.empty())
         ROS_ERROR("Global Frame: %s Plan Frame size %d: %s\n", global_frame.c_str(), (unsigned int)global_plan.size(), global_plan[0].header.frame_id.c_str());
 
       return false;
